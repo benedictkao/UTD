1d8bd70fd8c54e08e1c5a30937c17b8f5265d707
chris-nee <lingngeehung@gmail.com>
2023-05-11 22:32:26 +0800

    added timer, format

--- UTD/UTD.cpp
+++ UTD/UTD.cpp
@@ -1,12 +1,30 @@
 ﻿// UTD.cpp : Defines the entry point for the application.
 //
+#include <unistd.h>
+
 #include "UTD.h"
+#include "utils/timer.h"
 
 int main()
 {
-	utd::array<int, 2> a;
-	a.set(0, 0);
-	a.set(1, 1);
-	std::cout << a.get(0) << ", " << a.get(1) << std::endl;
-	return 0;
+        utd::array<int, 2> a;
+
+        utils::timer clock_watch;
+
+        clock_watch.start();
+        // sleep(1); // for testing watch
+
+        for (int i = 0; i < 10000; i++)
+        {
+                a.set(0, 0);
+                a.set(1, 1);
+        }
+
+        clock_watch.stop();
+
+        std::cout << a.get(0) << ", " << a.get(1) << std::endl;
+
+        clock_watch.print_reading();
+
+        return 0;
 }
