0b95159475aec983fe3985b30210e0b314ee5ba9
chris-nee <lingngeehung@gmail.com>
2023-05-14 16:46:11 +0700

    organise main file code testing

--- UTD/UTD.cpp
+++ UTD/UTD.cpp
@@ -6,40 +6,51 @@
 
 #include "utils/timer.h"
 
-void main_test() {
+void test_code() {
         utd::array<int, 2> a;
 
         for (int i = 0; i < 10000; i++) {
                 a.set(0, 0);
                 a.set(1, 1);
         }
+
+        // std::cout << a.get(0) << ", " << a.get(1) << std::endl;
 };
 
-int main() {
-        utd::array<int, 2> a;
-
-        /* Timer class method */
+void test_with_timer_class() {
         std::cout << "TIMER CLASS METHOD"
                   << "\n";
 
-        utils::timer clock_watch;
+        utils::Timer clock_watch;
         clock_watch.start();
 
-        for (int i = 0; i < 10000; i++) {
-                a.set(0, 0);
-                a.set(1, 1);
-        }
+        test_code();
 
         clock_watch.stop();
         clock_watch.print_reading(utils::MICRO_SEC);
+}
 
-        std::cout << a.get(0) << ", " << a.get(1) << std::endl;
-
-        /* Timer method */
+void test_with_time_method() {
         std::cout << "TIMER METHOD"
                   << "\n";
 
-        utils::time(main_test, utils::MICRO_SEC);
+        utils::time(test_code, utils::MICRO_SEC);
+}
+
+void test_with_timer_class_raii() {
+        std::cout << "TIMER CLASS RAII METHOD"
+                  << "\n";
+
+        utils::Timer_raii timer_raii("Testing set array", utils::MICRO_SEC);
+        test_code();
+}
+
+int main() {
+        test_with_timer_class();
+
+        test_with_time_method();
+
+        test_with_timer_class_raii();
 
         return 0;
 };
