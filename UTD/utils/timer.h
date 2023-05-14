96ea0beed0d55f9eeabbaefbc23d2c0b71c80c44
chris-nee <lingngeehung@gmail.com>
2023-05-14 16:12:12 +0700

    time function util

--- UTD/utils/timer.h
+++ UTD/utils/timer.h
@@ -4,38 +4,51 @@
 #include <iostream>
 
 // Utility classes
-namespace utils
-{
-        // Helper Timer Class using std::chrono
-        class timer
-        {
-        private:
-                std::chrono::time_point<std::chrono::steady_clock> start_time;
-                std::chrono::time_point<std::chrono::steady_clock> end_time;
+namespace utils {
+// Helper Timer Class using std::chrono
+class timer {
+       private:
+        std::chrono::time_point<std::chrono::steady_clock> start_time;
+        std::chrono::time_point<std::chrono::steady_clock> end_time;
 
-                void reset()
-                {
-                        start_time = std::chrono::steady_clock::now();
-                        end_time = std::chrono::steady_clock::now();
-                }
+        void reset() {
+                start_time = std::chrono::steady_clock::now();
+                end_time = std::chrono::steady_clock::now();
+        }
 
-        public:
-                timer() { reset(); }
+       public:
+        timer() { reset(); }
 
-                void start() { reset(); }
+        void start() { reset(); }
 
-                void stop() { end_time = std::chrono::steady_clock::now(); }
+        void stop() { end_time = std::chrono::steady_clock::now(); }
 
-                void print_reading()
-                {
-                        const auto interval =
-                            std::chrono::duration_cast<std::chrono::microseconds>(
-                                end_time - start_time);
+        void print_reading() {
+                const auto interval =
+                    std::chrono::duration_cast<std::chrono::microseconds>(
+                        end_time - start_time);
 
-                        std::cout << "Time Reading: " << interval.count()
-                                  << "microseconds"
-                                  << "\n";
-                }
-        };
+                std::cout << "Time Reading: " << interval.count()
+                          << " microseconds"
+                          << "\n";
+        }
+};
 
-} // namespace utils
+void time(void (*func)()) {
+        std::chrono::time_point<std::chrono::steady_clock> start_time =
+            std::chrono::steady_clock::now();
+
+        func();
+
+        std::chrono::time_point<std::chrono::steady_clock> end_time =
+            std::chrono::steady_clock::now();
+
+        const auto interval =
+            std::chrono::duration_cast<std::chrono::microseconds>(end_time -
+                                                                  start_time);
+
+        std::cout << "Time Reading: " << interval.count() << " microseconds"
+                  << "\n";
+};
+
+}  // namespace utils
