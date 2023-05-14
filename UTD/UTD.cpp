898733cb66296bd8128367aa971323b4bdba0bbc
chris-nee <lingngeehung@gmail.com>
2023-05-14 16:12:39 +0700

    apply time method

--- UTD/UTD.cpp
+++ UTD/UTD.cpp
@@ -1,30 +1,45 @@
 ﻿// UTD.cpp : Defines the entry point for the application.
-//
-#include <unistd.h>
 
 #include "UTD.h"
+
+#include <unistd.h>
+
 #include "utils/timer.h"
 
-int main()
-{
+void main_test() {
         utd::array<int, 2> a;
 
+        for (int i = 0; i < 10000; i++) {
+                a.set(0, 0);
+                a.set(1, 1);
+        }
+};
+
+int main() {
+        utd::array<int, 2> a;
+
+        /* Timer class method */
+        std::cout << "TIMER CLASS METHOD"
+                  << "\n";
+
         utils::timer clock_watch;
-
         clock_watch.start();
-        // sleep(1); // for testing watch
 
-        for (int i = 0; i < 10000; i++)
-        {
+        for (int i = 0; i < 10000; i++) {
                 a.set(0, 0);
                 a.set(1, 1);
         }
 
         clock_watch.stop();
+        clock_watch.print_reading();
 
         std::cout << a.get(0) << ", " << a.get(1) << std::endl;
 
-        clock_watch.print_reading();
+        /* Timer method */
+        std::cout << "TIMER METHOD"
+                  << "\n";
+
+        utils::time(main_test);
 
         return 0;
-}
+};
