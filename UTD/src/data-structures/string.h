0d4246394879268a0f9df0c1ed1e22768c375f8b
chris-nee <lingngeehung@gmail.com>
2023-07-13 19:48:50 +0800

    recover string

---
+++ UTD/src/data-structures/string.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <cstring> // should this be removed?
+#include <iostream>
+
+namespace utd
+{
+	class string
+	{
+	private:
+		char* inner_array = nullptr;
+		size_t arr_size = 0;
+		size_t str_size = 0;
+
+	public:
+
+		string() = default;
+
+		string(const char* s);
+
+		string(const string& s);
+
+		string(string&& s) noexcept;
+
+		string& operator=(const string& s);
+
+		string& operator=(string&& s) noexcept;
+
+		~string();
+
+		char& operator [](size_t index);
+
+		const char& operator [](size_t index) const;
+
+		size_t size() const;
+
+		const char* c_str() const;
+
+		void reserve(size_t new_size);
+
+		string substr(size_t start, size_t len);
+
+		string operator+(const string& rhs) const;
+
+		string& operator+=(const string& rhs);
+
+		string operator+(const char& rhs) const;
+
+		string& operator+=(const char& rhs);
+
+		friend bool operator==(const string& lhs, const string& rhs);
+
+		friend std::ostream& operator<<(std::ostream& os, const string& s);
+	};
+}
+
