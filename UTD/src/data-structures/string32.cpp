9d1e74a692236a9a3803212cbc9c1f2f8becf8e6
chris-nee <lingngeehung@gmail.com>
2023-07-13 18:38:12 +0800

    test

--- UTD/src/data-structures/string32.cpp
+++ UTD/src/data-structures/string32.cpp
@@ -1,21 +1,21 @@
 #include "string32.h"
 
-#include <cstring>
 
 /*
-* Size of string is 32:
-* 
-* Large string:
-* - 8 bytes for data pointer -> points to heap memory
-* - 8 bytes for size
-* - 8 bytes for capacity
-* - 8 bytes spare space
-* 
-* Small string:
-* - 8 bytes for data pointer -> points to capacity (which is used to start char array in small string)
-* - 8 bytes for size
-* - 16 bytes for char array (last element must be null terminator)
-*/
+ * Size of string is 32:
+ *
+ * Large string:
+ * - 8 bytes for data pointer -> points to heap memory
+ * - 8 bytes for size
+ * - 8 bytes for capacity
+ * - 8 bytes spare space
+ *
+ * Small string:
+ * - 8 bytes for data pointer -> points to capacity (which is used to start char
+ * array in small string)
+ * - 8 bytes for size
+ * - 16 bytes for char array (last element must be null terminator)
+ */
 static constexpr uint8_t SMALL_STRING_MAX_LENGTH{ 15 };
 
 static constexpr size_t CHAR_BUFFER_SIZE{ 7 };
@@ -23,275 +23,266 @@ static constexpr size_t CHAR_BUFFER_SIZE{ 7 };
 // Private Methods
 
 bool utd::string32::isLargeString() const {
-        return _flags.isLargeString;
+  return _flags.isLargeString;
 }
 
 void utd::string32::setLargeString() {
-        _flags.isLargeString = true;
+  _flags.isLargeString = true;
 }
 
 /*
-* For small strings, _capacity, _char_buffer and _flags are used to store the character data and null terminator
-*/
+ * For small strings, _capacity, _char_buffer and _flags are used to store the
+ * character data and null terminator
+ */
 void utd::string32::pointToInSituMemory() {
-        _data = (char*) &_capacity;
+  _data = (char*) &_capacity;
 }
 
 
 // Constructors
 
 utd::string32::string32() {
-        // TODO: can we improve this by using a global variable for the empty string?
-        _size     = 0;
-        _capacity = 0;
-        pointToInSituMemory();
+  // TODO: can we improve this by using a global variable for the empty string?
+  _size     = 0;
+  _capacity = 0;
+  pointToInSituMemory();
 }
 
 utd::string32::string32(const char* s) {
-        _size = strlen(s);
-        if (_size > SMALL_STRING_MAX_LENGTH) {
-          _capacity = _size + 1;
-          _data     = new char[_capacity];
-          setLargeString();
-        } else {
-          pointToInSituMemory();
-        }
-        strcpy(_data, s);
+  _size = strlen(s);
+  if (_size > SMALL_STRING_MAX_LENGTH) {
+    _capacity = _size + 1;
+    _data     = new char[_capacity];
+    setLargeString();
+  } else {
+    pointToInSituMemory();
+  }
+  strcpy(_data, s);
 }
 
 utd::string32::string32(const string32& s) {
-        _size     = s._size;
-        _capacity = s._capacity;
-        memcpy(_char_buffer, s._char_buffer, CHAR_BUFFER_SIZE);
-        _flags = s._flags;
-        if (_size > SMALL_STRING_MAX_LENGTH) {
-          _data = new char[_capacity];
-          strcpy(_data, s._data);
-        } else {
-          pointToInSituMemory();
-        }
+  _size     = s._size;
+  _capacity = s._capacity;
+  memcpy(_char_buffer, s._char_buffer, CHAR_BUFFER_SIZE);
+  _flags = s._flags;
+  if (_size > SMALL_STRING_MAX_LENGTH) {
+    _data = new char[_capacity];
+    strcpy(_data, s._data);
+  } else {
+    pointToInSituMemory();
+  }
 }
 
 utd::string32::string32(string32&& s) noexcept {
-        _size     = s._size;
-        _capacity = s._capacity;
-        memcpy(_char_buffer, s._char_buffer, CHAR_BUFFER_SIZE);
-        _flags = s._flags;
+  _size     = s._size;
+  _capacity = s._capacity;
+  memcpy(_char_buffer, s._char_buffer, CHAR_BUFFER_SIZE);
+  _flags = s._flags;
 
-        if (_size > SMALL_STRING_MAX_LENGTH) {
-          _data = s._data;
-        } else {
-          pointToInSituMemory();
-        }
-        
-        s._data     = nullptr;
-        s._size     = 0;
-        s._capacity = 0;
-        memset(s._char_buffer, 0, CHAR_BUFFER_SIZE);
-        s._flags.reset();
+  if (_size > SMALL_STRING_MAX_LENGTH) {
+    _data = s._data;
+  } else {
+    pointToInSituMemory();
+  }
+
+  s._data     = nullptr;
+  s._size     = 0;
+  s._capacity = 0;
+  memset(s._char_buffer, 0, CHAR_BUFFER_SIZE);
+  s._flags.reset();
 }
 
 utd::string32& utd::string32::operator=(const string32& s) {
-    if (_size > SMALL_STRING_MAX_LENGTH) {
-        delete[] _data;
-    }
-    _size     = s._size;
-    _capacity = s._capacity;
-    memcpy(_char_buffer, s._char_buffer, CHAR_BUFFER_SIZE);
-    _flags = s._flags;
+  if (_size > SMALL_STRING_MAX_LENGTH) {
+    delete[] _data;
+  }
+  _size     = s._size;
+  _capacity = s._capacity;
+  memcpy(_char_buffer, s._char_buffer, CHAR_BUFFER_SIZE);
+  _flags = s._flags;
 
-	if (_size > SMALL_STRING_MAX_LENGTH) {
-        _data = new char[_capacity];
-        strcpy(_data, s._data);
-    } else {
-        pointToInSituMemory();
-    }
-    return *this;
+  if (_size > SMALL_STRING_MAX_LENGTH) {
+    _data = new char[_capacity];
+    strcpy(_data, s._data);
+  } else {
+    pointToInSituMemory();
+  }
+  return *this;
 }
 
 utd::string32& utd::string32::operator=(string32&& s) noexcept {
-    if (_size > SMALL_STRING_MAX_LENGTH) {
-        delete[] _data;
-    }
-    _size     = s._size;
-    _capacity = s._capacity;
-    memcpy(_char_buffer, s._char_buffer, CHAR_BUFFER_SIZE);
-    _flags = s._flags;
-    
-    if (_size > SMALL_STRING_MAX_LENGTH) {
-        _data = s._data;
-    } else {
-        pointToInSituMemory();
-    }
+  if (_size > SMALL_STRING_MAX_LENGTH) {
+    delete[] _data;
+  }
+  _size     = s._size;
+  _capacity = s._capacity;
+  memcpy(_char_buffer, s._char_buffer, CHAR_BUFFER_SIZE);
+  _flags = s._flags;
 
-    s._data = nullptr;
-    s._size = 0;
-    s._capacity = 0;
-    memset(s._char_buffer, 0, CHAR_BUFFER_SIZE);
-    s._flags.reset();
-    return *this;
+  if (_size > SMALL_STRING_MAX_LENGTH) {
+    _data = s._data;
+  } else {
+    pointToInSituMemory();
+  }
+
+  s._data     = nullptr;
+  s._size     = 0;
+  s._capacity = 0;
+  memset(s._char_buffer, 0, CHAR_BUFFER_SIZE);
+  s._flags.reset();
+  return *this;
 }
 
 
-utd::string32::~string32()
-{
-	if (isLargeString())
-	{
-		delete[] _data;
-	}
+utd::string32::~string32() {
+  if (isLargeString()) {
+    delete[] _data;
+  }
 }
 
 // Public Methods
 
-uint64_t utd::string32::size() const
-{
-	return _size;
+uint64_t utd::string32::size() const {
+  return _size;
 }
 
-uint64_t utd::string32::capacity() const
-{
-	if (isLargeString())
-	{
-		return _capacity;
-	}
-	else
-	{
-		return SMALL_STRING_MAX_LENGTH + 1;
-	}
+uint64_t utd::string32::capacity() const {
+  if (isLargeString()) {
+    return _capacity;
+  } else {
+    return SMALL_STRING_MAX_LENGTH + 1;
+  }
 }
 
-const char* utd::string32::c_str() const
-{
-    return _data;
+const char* utd::string32::c_str() const {
+  return _data;
 }
 
 /*
- * Will reserve space for a n-size string. Means that at least n+1 memory space will be allocated to include the null terminator.
+ * Will reserve space for a n-size string. Means that at least n+1 memory space
+ * will be allocated to include the null terminator.
  */
-void utd::string32::reserve(uint64_t n)
-{
-	if (n <= SMALL_STRING_MAX_LENGTH) return;
+void utd::string32::reserve(uint64_t n) {
+  if (n <= SMALL_STRING_MAX_LENGTH)
+    return;
 
-	char* new_data = new char[n + 1];
+  char* new_data = new char[n + 1];
 
-	if (n < _size) _size = n;
-	memcpy(new_data, _data, _size + 1);
-	if (isLargeString()) delete[] _data;
-	_capacity = n + 1;
+  if (n < _size)
+    _size = n;
+  memcpy(new_data, _data, _size + 1);
+  if (isLargeString())
+    delete[] _data;
+  _capacity = n + 1;
 
-	_data = new_data;
-	*(_data + _size) = 0;	// regenerate the null terminator
+  _data            = new_data;
+  *(_data + _size) = 0; // regenerate the null terminator
 
-	setLargeString();
+  setLargeString();
 }
 
 // Operators
 
-char& utd::string32::operator[](uint64_t index)
-{
-	return _data[index];
+char& utd::string32::operator[](uint64_t index) {
+  return _data[index];
 }
 
 const char& utd::string32::operator[](uint64_t index) const {
-        return _data[index];
+  return _data[index];
 }
 
-utd::string32 utd::string32::operator+(const utd::string32& rhs) const
-{
-	utd::string32 lhs;
-	lhs._size = this->_size + rhs._size;
+utd::string32 utd::string32::operator+(const utd::string32& rhs) const {
+  utd::string32 lhs;
+  lhs._size = this->_size + rhs._size;
 
-	if (lhs._size > SMALL_STRING_MAX_LENGTH) {
-                lhs._capacity = lhs._size + 1;
-                lhs._data     = new char[_capacity];
-                lhs.setLargeString();
-	} else {
-                lhs.pointToInSituMemory();
-	}
-	strcpy(lhs._data, this->_data);
-	strcpy(lhs._data + this->_size, rhs._data);
-	return lhs;
+  if (lhs._size > SMALL_STRING_MAX_LENGTH) {
+    lhs._capacity = lhs._size + 1;
+    lhs._data     = new char[_capacity];
+    lhs.setLargeString();
+  } else {
+    lhs.pointToInSituMemory();
+  }
+  strcpy(lhs._data, this->_data);
+  strcpy(lhs._data + this->_size, rhs._data);
+  return lhs;
 }
 
 utd::string32& utd::string32::operator+=(const utd::string32& rhs) {
-        const uint64_t new_size = _size + rhs._size;
-        if (isLargeString()) {
-                if (new_size >= _capacity) {
-                  _capacity = new_size + 1;
-                  char* new_data = new char[_capacity];
-                  strcpy(new_data, _data);
-                  delete[] _data;
-                  _data = new_data;
-                }
-        } else if (new_size > SMALL_STRING_MAX_LENGTH) {
-                const uint64_t new_capacity = new_size + 1;
-                char*          new_data     = new char[new_capacity];
-                strcpy(new_data, _data);
-                _data     = new_data;
-                _capacity = new_capacity;
-                setLargeString();
-        }
-        strcpy(_data + _size, rhs._data);
-        _size = new_size;
-		return *this;
+  const uint64_t new_size = _size + rhs._size;
+  if (isLargeString()) {
+    if (new_size >= _capacity) {
+      _capacity      = new_size + 1;
+      char* new_data = new char[_capacity];
+      strcpy(new_data, _data);
+      delete[] _data;
+      _data = new_data;
+    }
+  } else if (new_size > SMALL_STRING_MAX_LENGTH) {
+    const uint64_t new_capacity = new_size + 1;
+    char*          new_data     = new char[new_capacity];
+    strcpy(new_data, _data);
+    _data     = new_data;
+    _capacity = new_capacity;
+    setLargeString();
+  }
+  strcpy(_data + _size, rhs._data);
+  _size = new_size;
+  return *this;
 }
 
-utd::string32 utd::string32::operator+(const char& rhs) const
-{
-    utd::string32 lhs;
-    lhs._size = this->_size + 1;
+utd::string32 utd::string32::operator+(const char& rhs) const {
+  utd::string32 lhs;
+  lhs._size = this->_size + 1;
 
-	if (lhs._size > SMALL_STRING_MAX_LENGTH)
-	{
-                lhs._capacity = lhs._size + 1;
-                lhs._data     = new char[_capacity];
-                lhs.setLargeString();
-	}
-	else
-	{
-                lhs.pointToInSituMemory();
-	}
-	strcpy(lhs._data, this->_data);
-	*(lhs._data + this->_size) = rhs;			// replace null termination with rhs char
-	*(lhs._data + this->_size + 1) = 0;			// add back null termination
-	return lhs;
+  if (lhs._size > SMALL_STRING_MAX_LENGTH) {
+    lhs._capacity = lhs._size + 1;
+    lhs._data     = new char[_capacity];
+    lhs.setLargeString();
+  } else {
+    lhs.pointToInSituMemory();
+  }
+  strcpy(lhs._data, this->_data);
+  *(lhs._data + this->_size) = rhs;   // replace null termination with rhs char
+  *(lhs._data + this->_size + 1) = 0; // add back null termination
+  return lhs;
 }
 
 utd::string32& utd::string32::operator+=(const char& rhs) {
-        _size++;
-        if (isLargeString()) {
-                if (_size == _capacity) {
-                  char* new_data = new char[++_capacity];
-                  strcpy(new_data, _data);
-                  delete[] _data;
-                  _data = new_data;
-                }
-        } else if (_size > SMALL_STRING_MAX_LENGTH) {
-                const uint64_t new_capacity = _size + 1;
-                char*          new_data     = new char[new_capacity];
-                strcpy(new_data, _data);
-                _data     = new_data;
-                _capacity = new_capacity;
-                setLargeString();
-        }
-        *(_data + this->_size) = rhs;			// replace null termination with rhs char
-        *(_data + this->_size + 1) = 0;			// add back null termination
-        return *this;
+  _size++;
+  if (isLargeString()) {
+    if (_size == _capacity) {
+      char* new_data = new char[++_capacity];
+      strcpy(new_data, _data);
+      delete[] _data;
+      _data = new_data;
+    }
+  } else if (_size > SMALL_STRING_MAX_LENGTH) {
+    const uint64_t new_capacity = _size + 1;
+    char*          new_data     = new char[new_capacity];
+    strcpy(new_data, _data);
+    _data     = new_data;
+    _capacity = new_capacity;
+    setLargeString();
+  }
+  *(_data + this->_size)     = rhs; // replace null termination with rhs char
+  *(_data + this->_size + 1) = 0;   // add back null termination
+  return *this;
 }
 
-bool utd::operator==(const string32& lhs, const string32& rhs)
-{
-	if (lhs._size != rhs._size) return false;
+bool utd::operator==(const string32& lhs, const string32& rhs) {
+  if (lhs._size != rhs._size)
+    return false;
 
-	for (int i = 0; i < lhs._size; i++)
-		if (lhs[i] != rhs[i]) return false;
+  for (int i = 0; i < lhs._size; i++)
+    if (lhs[i] != rhs[i])
+      return false;
 
-	return true;
+  return true;
 }
 
 // Output Stream
 
 std::ostream& utd::operator<<(std::ostream& os, const utd::string32& s) {
-    os << s._data;
-    return os;
+  os << s._data;
+  return os;
 }
--- UTD/src/data-structures/string32.h
+++ UTD/src/data-structures/string32.h
@@ -1,81 +1,83 @@
 #pragma once
 
-#include <stdint.h>
+#include <cstring>
 #include <iostream>
+#include <stdint.h>
 
 namespace utd {
 
-	class string32 {
-	public:
-   
-	private:
-          /*
-		  * Since last character of a string is always null (0), the last byte is guaranteed to either be unused or have value of zero.
-		  * This leaves us with one byte (8 bits) of memory to use for boolean flags. However, all flag values have to be false when data is stored in-situ
-		  * otherwise the last byte cannot be used as a null terminator if the small string capacity is full
-		  */
-          struct str_flags {
-          public:
-            bool isLargeString = false;
+  class string32 {
+  public:
+  private:
+    /*
+     * Since last character of a string is always null (0), the last byte is
+     * guaranteed to either be unused or have value of zero. This leaves us with
+     * one byte (8 bits) of memory to use for boolean flags. However, all flag
+     * values have to be false when data is stored in-situ otherwise the last
+     * byte cannot be used as a null terminator if the small string capacity is
+     * full
+     */
+    struct str_flags {
+    public:
+      bool isLargeString = false;
 
-		  private:
-            bool _unused_flags[7] = { false };
+    private:
+      bool _unused_flags[7] = { false };
 
-			public:
-            void reset() { memset((void*) this, 0, 1); }
-          };
+    public:
+      void reset() { memset((void*) this, 0, 1); }
+    };
 
 
-          char*            _data;
-          uint64_t         _size;
-          uint64_t         _capacity;
-          char             _char_buffer[7] = { 0 };
-          str_flags        _flags;
-		  
-		  bool isLargeString() const;
-		  
-		  void setLargeString();
-		  
-		  void pointToInSituMemory();
+    char*     _data;
+    uint64_t  _size;
+    uint64_t  _capacity;
+    char      _char_buffer[7] = { 0 };
+    str_flags _flags;
 
-	public:
+    bool isLargeString() const;
 
-		string32();
+    void setLargeString();
 
-		string32(const char*);
+    void pointToInSituMemory();
 
-		string32(const string32&);
-		
-		string32(string32&&) noexcept;
-		
-		string32& operator=(const string32&);
+  public:
+    string32();
 
-		string32& operator=(string32&&) noexcept;
+    string32(const char*);
 
-		~string32();
+    string32(const string32&);
 
-		uint64_t size() const;
+    string32(string32&&) noexcept;
 
-		uint64_t capacity() const;
+    string32& operator=(const string32&);
 
-		const char* c_str() const;
+    string32& operator=(string32&&) noexcept;
 
-		void reserve(uint64_t);
+    ~string32();
 
-		char& operator [](uint64_t);
+    uint64_t size() const;
 
-        const char& operator[](uint64_t) const;
+    uint64_t capacity() const;
 
-		string32 operator+(const string32&) const;
+    const char* c_str() const;
 
-		string32& operator+=(const string32&);
+    void reserve(uint64_t);
 
-		string32 operator+(const char&) const;
+    char& operator[](uint64_t);
 
-		string32& operator+=(const char&);
+    const char& operator[](uint64_t) const;
 
-		friend bool operator==(const string32&, const string32&);
+    string32 operator+(const string32&) const;
 
-		friend std::ostream& operator<<(std::ostream&, const string32&);
-	};
-}
+    string32& operator+=(const string32&);
+
+    string32 operator+(const char&) const;
+
+    string32& operator+=(const char&);
+
+    friend bool operator==(const string32&, const string32&);
+
+    friend std::ostream& operator<<(std::ostream&, const string32&);
+  };
+} // namespace utd
